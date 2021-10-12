9e31780f94d5780209e70726e4fca9a64ed2f0eb
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-10-13 02:09:52 +0300

    solcsfc

---
+++ codechallenge3/Binarysearch.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class Binarysearch {
+
+    public static void main(String[] args) {
+        int [] arr1={1,2,4,8,9,3};
+        System.out.println(arr1.length);
+    }
+public static int binaryfind(int [] arr,int x){
+
+        int index=-1;
+         Arraylist <integer> newArr=new Arraylist <>();
+         for(int j=0;j<)
+        while (index == -1){
+            int middele=newArr.length/2;
+
+            if(x>middele){
+             for (int i= middele;i<arr.length;i++){
+                 newArr[i]=arr[i];
+
+             }
+            }
+
+        }
+
+}
+
+
+}
